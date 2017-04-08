f9da0eae2436598645e566e35a0308861e8ac4be
Kris Kane <kris@skilldistillery.com>
2017-04-07 20:53:01 -0600

    add sweet 1984 reference

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,11 @@
 public class HelloWorld {
   public static void main(String args[]) {
     System.out.println("Hello fello git users");
+
+    if ((2+2) == 5) {
+
+    } else {
+      System.out.println("Big brother is wrong");
+    }
   }
 }
