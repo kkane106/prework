36b7812d9ae2405b6a66df2b02f6f5251ae2536a
Kris Kane <kris@skilldistillery.com>
2017-04-07 20:43:30 -0600

    change message

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,7 @@
 public class HelloWorld {
   public static void main(String args[]) {
-    System.out.println("Hello World");
+    System.out.println("Hello fello git users");
+
+    System.out.println("the world says hello");
   }
 }
